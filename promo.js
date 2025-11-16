5d566fd39f39a801016f60bca4f4483c99315509
Chrstxxan <chris.delucca.2006@gmail.com>
2025-11-16 00:12:28 -0300

    start heating bf

---
+++ promo.js
@@ -0,0 +1,17 @@
+///////////////////////
+// INÍCIO BLACK FRIDAY
+// Config global de promoção
+///////////////////////
+
+window.PROMO = {
+  active: true,              // ligar/desligar promo
+  discountPercentage: 20,    // Esquenta Black Friday (20%)
+  freeShipping: false        // Esquenta não tem frete grátis
+};
+
+// No dia 28 troque para:
+// window.PROMO = { active: true, discountPercentage: 30, freeShipping: true };
+
+///////////////////////
+// FIM BLACK FRIDAY
+///////////////////////
